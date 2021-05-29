eba540e81471bf147e6c90d9fdea45a937d49bdc
Arti <arti.iosdev@gmail.com>
2021-05-29 00:22:05 -0700

    added build folder

---
+++ build/precache-manifest.9a810558337e04fbc2c4ae667600383f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0aebf78a1b61fb802b7b41c3a81e17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d80760bb2c60cd117cc",
+    "url": "/static/css/2.02adba32.chunk.css"
+  },
+  {
+    "revision": "f7826a7c90e80e6bf949",
+    "url": "/static/css/main.39982285.chunk.css"
+  },
+  {
+    "revision": "7d80760bb2c60cd117cc",
+    "url": "/static/js/2.e4fe6e8b.chunk.js"
+  },
+  {
+    "revision": "223be75f6809993b56a94be00d710bed",
+    "url": "/static/js/2.e4fe6e8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7826a7c90e80e6bf949",
+    "url": "/static/js/main.a8bffd93.chunk.js"
+  },
+  {
+    "revision": "c46c97987879c7eab527",
+    "url": "/static/js/runtime-main.8dd9c097.js"
+  }
+]);
